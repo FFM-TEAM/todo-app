61862de42a27ca1dcf4730c7073981dc856dbf1b
yuseungrii <siso1210@gmail.com>
2021-11-08 23:20:43 +0900

    commit1

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+
+import Todos from './components/Todos';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <React.Fragment>
+        <div className="container">
+          <div className="startsTopRight">
+            <div className="startsTopLeft">
+              <div className="headerDiv">What's new mission?</div>
+              <Todos>
+
+              </Todos>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
     </div>
   );
 }
---
+++ src/components/Todos.js
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Todos = props => {
+    // return 
+    return "하이";
+};
+
+export default Todos;
