5bc664e32fd8ac8822c24e3463cb0e5710a2ba56
yuseungrii <siso1210@gmail.com>
2021-11-15 22:11:49 +0900

    commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,67 @@
 import React, { useState, useEffect } from 'react';
-import './App.css';
+import axios from "axios";
+import uuid from "uuid";
+// import "./App.css";
 
+import AddTodo from "./components/AddTodo";
+import Toggle from "./components/Toggle";
 import Todos from './components/Todos';
 
 function App() {
+
+  const [state, setState] = useState({
+    todos: []
+  });
+
+  // { id: '', status: '', title: '' } 
+
+  // 테스트 데이터, API 불러오기
+  const getData = () => {
+    axios
+      .get("https://killsanghyuck.github.io/prography_5th_front/todoDummy.json")
+      .then(res => {
+        console.log(res, 'res');
+        setState({ todos: res.data.body });
+      });
+  };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  // Add Todo
+  const addTodo = title => {
+    const newTodo = {
+      title,
+      id: uuid.v4(),
+      status: "todo"
+    };
+    setState({ todos: [...state.todos, newTodo] });
+  };
+
+  // Delete Todo
+  const delTodo = id => {
+
+
+    setState({ todos: [...state.todos.filter(todo => todo.id !== id)] });
+  };
+
+  //취소선
+  const markComplete = id => {
+    setState({
+      todos: state.todos.map(todo => {
+        if (todo.id === id) {
+          if (todo.status === "todo") {
+            todo.status = "complete";
+          } else {
+            todo.status = "todo";
+          }
+        }
+        return todo;
+      })
+    });
+  };
+
   return (
     <div className="App">
       <React.Fragment>
@@ -11,9 +69,18 @@ function App() {
           <div className="startsTopRight">
             <div className="startsTopLeft">
               <div className="headerDiv">What's new mission?</div>
-              <Todos>
-
-              </Todos>
+              <AddTodo addTodo={addTodo} />
+              App!
+              <Toggle>
+                // toggle children
+                <div className="todoListDIv">
+                  <Todos
+                    todos={state.todos}
+                    markComplete={markComplete}
+                    delTodo={delTodo}
+                  />
+                </div>
+              </Toggle>
             </div>
           </div>
         </div>
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -1,8 +1,24 @@
 import React from "react";
+import TodoItem from "./TodoItem";
+import PropTypes from "prop-types";
 
 const Todos = props => {
-    // return 
-    return "하이";
+    console.log(props, 'props');
+    return props.todos.map(todo => (
+        <TodoItem
+            key={todo.id}
+            todo={todo}
+            markComplete={props.markComplete}
+            delTodo={props.delTodo}
+        />
+    ));
+
+};
+
+Todos.propTypes = {
+    todos: PropTypes.array.isRequired,
+    markComplete: PropTypes.func.isRequired,
+    delTodo: PropTypes.func.isRequired
 };
 
 export default Todos;
